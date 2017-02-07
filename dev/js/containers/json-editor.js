8176a386fa41de535004720629d2e8cc8b2f95cc
Everett Franco <everett.franco@gmail.com>
2017-02-07 16:28:25 -0500

    Uses this version: https://github.com/josdejong/jsoneditor
    This was a little weird setting up, it didn’t install into webpack
    seamlessly and still is a bit messy to get working.

--- dev/js/containers/json-editor.js
+++ dev/js/containers/json-editor.js
@@ -4,13 +4,49 @@ import {connect} from 'react-redux';
 import {editUserInfo} from '../actions/index';
 import TextInput from '../components/text-input';
 import Immutable, {Map} from 'immutable';
-
+// import JsonEditorTool from 'jsoneditor';
+import JSONEditor from 'jsoneditor';
+// import 'jsoneditor.css';
+// const json = require('jsoneditor');
 
 class JsonEditor extends Component {
 
 	constructor(props) {
 		super(props);
+
+		// console.log( this.refs );
 	}
+
+	componentDidMount() {
+
+    	const containerText = this.refs['json-editor-text'];
+        const optionsText = {
+        	"mode": "text",
+    		"indentation": 4
+        };
+        const editorText = new JSONEditor(containerText, optionsText);
+
+
+        const containerTree = this.refs['json-editor-tree'];
+        const optionsTree = {
+        	"mode": "tree",
+        };
+        const editorTree = new JSONEditor(containerTree, optionsTree);
+
+        // const json = {
+        //     "Array": [1, 2, 3],
+        //     "Boolean": true,
+        //     "Null": null,
+        //     "Number": 123,
+        //     "Object": {"a": "b", "c": "d"},
+        //     "String": "Hello World"
+        // };
+
+
+
+        editorTree.set(this.props);
+        editorText.set(this.props);
+  	}
 
 	render() {
 		return (
@@ -20,7 +56,21 @@ class JsonEditor extends Component {
                     <div className="panel-heading">
                         <h3 className='panel-title'>Editor</h3>
                     </div>
-                    <div className="panel-body"></div>
+                    <div className="panel-body">
+                    	<div
+                    		ref='json-editor-text'
+                    		className='json-editor-container'
+                    		style={{width:'100%', height: 400}}>
+                    	</div>
+
+                    	<hr/>
+
+                    	<div
+                    		ref='json-editor-tree'
+                    		className='json-editor-container'
+                    		style={{width:'100%', height: 400}}>
+                    	</div>
+                    </div>
                 </div>
 			</div>
 		);
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,29 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
+                test2: /\.js$/,
+                loaders: ['shebang'],
+                include: '/node_modules/nodent/'
+            },
+            {
+                test: /\.json$/,
+                loaders: ['json-loader'],
+                // include: '/node_modules/'
+                // exclude: /node_modules/
+            },
+            {
+                test: /\.svg/,
+                loader: 'svg-url-loader',
+                exclude: /node_modules/
+            },
+            {
                 test: /\.scss/,
                 loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.css/,
+                loader: 'style-loader!css-loader',
+                // include: '/node_modules/jsoneditor/'
             }
         ]
     },
